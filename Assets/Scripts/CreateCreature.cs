7ba6c737793d6416c34bdd9e61fcd264ba14368f
CosmicEgg <johnholmberg_@hotmail.com>
2020-04-08 13:51:29 +0200

    Test

---
+++ Assets/Scripts/CreateCreature.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCreature : MonoBehaviour
+{
+    public List<Node> nodes;
+    public List<Edge> edges;
+    int numOfNodes;
+    Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numOfNodes = Random.Range(0, 11);
+        startPosition = Vector3.zero;
+        nodes = new List<Node>();
+        edges = new List<Edge>();
+
+        for (int i = 0; i < numOfNodes; i++)
+        {
+            nodes.Add(new Node());
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class Node
+{
+    GameObject segment = GameObject.CreatePrimitive(PrimitiveType.Cube);
+}
+
+public class Edge
+{
+
+}
+
