bfe8cf116705345bd0bcb48ccf844248ca40639d
Adrian <adrian.carlstrom@gmail.com>
2020-04-18 15:23:42 +0200

    Lagt till Object

---
+++ Assets/Scripts/AddForce.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+    Rigidbody rb;
+    Transform tr;
+    HingeJoint hJoint;
+    JointMotor motor;
+    float targetVelocity = 1000;
+    float force = 1000;
+
+    void Start()
+    {
+        if (TryGetComponent<HingeJoint>(out hJoint))
+        {
+            motor = hJoint.motor;
+        }
+
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.DrawRay(tr.position, tr.forward);
+        //Left joint 
+         if (Input.GetKey(KeyCode.X))
+        {
+            hJoint.useMotor = true;
+            motor.targetVelocity = targetVelocity;
+            motor.force = force;
+            hJoint.motor = motor;
+        }
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            hJoint.useMotor = true;
+            motor.targetVelocity = -targetVelocity;
+            motor.force = force;
+            hJoint.motor = motor;
+        }
+    }
+}
